6ff30c8d2cc5995e5019ab6131fe295cd6c1c639
Djezzar Moncef <djezzarmonsef@gmail.com>
2023-06-23 21:45:27 +0100

    learned how to compile a TypeScript file

---
+++ programming/TypeScript/01 intro/intro.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greeting = "Hello!";
+console.log(greeting);
+//to compile this file run the command tsc intro.ts
---
+++ programming/TypeScript/01 intro/intro.ts
@@ -0,0 +1,6 @@
+let greeting: string = "Hello!";
+console.log(greeting);
+
+export {};
+
+//to compile this file run the command tsc intro.ts
