47b841838355f84f617355e8d4738cf63a6a08da
Djezzar Moncef <djezzarmonsef@gmail.com>
2023-07-01 10:59:34 +0100

    learned how the useState hook works

--- programming/ReactJS and TailwindCSS/Practice app/src/Content.tsx
+++ programming/ReactJS and TailwindCSS/Practice app/src/Content.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+
 export const Content = () => {
-  const randomName = (): string => {
+  const [name, setName] = useState(``);
+  const randomName = (): void => {
     const names = [`Ahmed`, `Mohammed`, `Ali`];
-    return names[Math.floor(Math.random() * 3)];
+    setName(names[Math.floor(Math.random() * 3)]);
   };
 
   return (
-    <main className="flex items-center justify-center h-full">
-      <h2>Welcome {randomName()}!</h2>
+    <main className="flex flex-col items-center justify-center h-full">
+      <h2>Welcome {name}!</h2>
+      <button
+        className="bg-red-400 hover:bg-red-500 rounded-md py-1 px-2 my-2 text-red-50"
+        onClick={randomName}
+      >
+        Change Name
+      </button>
     </main>
   );
 };
