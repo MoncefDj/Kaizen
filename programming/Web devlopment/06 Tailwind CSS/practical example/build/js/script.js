444fd396c2ff6f7a790fc7fb487605fccd2067ce
Djezzar Moncef <djezzarmonsef@gmail.com>
2023-08-22 18:25:58 +0100

    practical example

--- programming/Web devlopment/06 Tailwind CSS/practical example/build/js/script.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const toggleDark = document.getElementById(`toggleDarkButton`);
-toggleDark === null || toggleDark === void 0 ? void 0 : toggleDark.addEventListener(`click`, () => {
-    const classList = document.documentElement.classList;
-    classList.contains(`dark`) ? classList.remove(`dark`) : classList.add(`dark`);
-});
--- programming/Web devlopment/06 Tailwind CSS/practical example/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./build/**/*.{html,js}"],
-  darkMode: "class",
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
